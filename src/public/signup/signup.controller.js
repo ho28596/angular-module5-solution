4e7fe492fbbecf3858e80d7ec7cb74b9a206cb53
ho28596 <fabio.borghetto@bancobpm.it>
2026-03-24 15:17:46 +0100

    Update signup.controller.js

--- src/public/signup/signup.controller.js
+++ src/public/signup/signup.controller.js
@@ -12,6 +12,9 @@ function SignUpController(allMenuItems) {
 
   for (var category in allMenuItems) {
     console.log(category);
+    console.log(category.menu_items);
+    console.log(menu_items);
+    
     for (item in category.menu_items) {
       if (item.short_name == "A1") {
         console.log(item.short_name + " -> " + item.description)
