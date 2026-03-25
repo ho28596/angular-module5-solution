b807a0fbbbfa2ace52bd73b1bea4fad11677e9d1
ho28596 <fabio.borghetto@bancobpm.it>
2026-03-25 10:15:38 +0100

    Update signup.controller.js

--- src/public/signup/signup.controller.js
+++ src/public/signup/signup.controller.js
@@ -12,17 +12,17 @@ function SignUpController(allMenuItems) {
 
   $ctrl.checkFavorite = function () {
     if ($ctrl.user.favDish.short_name) {
-      $ctrl.user.favDish.short_name.toUpperCase();
+      $ctrl.user.favDish.short_name = $ctrl.user.favDish.short_name.toUpperCase();
       for (var category in allMenuItems) {
         if (category=="A"){
           console.log($ctrl.allMenuItems[category].menu_items);
         }
         for (var item in $ctrl.allMenuItems[category].menu_items) {
           if (category=="A"){
-            console.log(item);
+            console.log($ctrl.allMenuItems[category].menu_items[item]);
           }
-          if (item.short_name == $ctrl.user.favDish.short_name) {
-            console.log("Items found: " + $ctrl.user.favDish.short_name);
+          if ($ctrl.allMenuItems[category].menu_items[item].short_name == $ctrl.user.favDish.short_name) {
+            console.log("Items found: " + $ctrl.allMenuItems[category].menu_items[item].description);
             return true;
           }            
         }
